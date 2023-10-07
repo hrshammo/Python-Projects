66b5bb5e498d80f60cde85a8241c44f376c00ab0
Hamim Reza <76872754+hrshammo@users.noreply.github.com>
2023-10-07 13:14:33 +0600

    Update 1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.py

--- LeetCode Problems/Hard/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.py
+++ LeetCode Problems/Hard/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.py
@@ -2,12 +2,18 @@ class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
         MOD = 10**9 + 7
 
-        # Define a recursive function to count the number of valid arrays.
+        # Create a memoization table with dimensions (pos, max_val, comparisons).
+        memo = {}
+
+        # Define a recursive function with memoization.
         def dp(pos, max_val, comparisons):
             if comparisons > k:
                 return 0
             if pos == n:
                 return 1 if comparisons == k else 0
+
+            if (pos, max_val, comparisons) in memo:
+                return memo[(pos, max_val, comparisons)]
 
             ans = 0
             for num in range(1, m + 1):
@@ -17,6 +23,7 @@ class Solution:
                     ans += dp(pos + 1, max_val, comparisons)
                 ans %= MOD
 
+            memo[(pos, max_val, comparisons)] = ans
             return ans
 
         return dp(0, 0, 0)
